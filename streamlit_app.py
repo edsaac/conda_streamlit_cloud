434e750f9a747703e934a8304bb77c46e4e79b12
edsaac <34405119+edsaac@users.noreply.github.com>
2023-02-20 15:05:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+import subprocess
+import numpy
 
 st.info("Sucess!")
+
+"# Run subprocess"
+code_1 = st.text_input("Code:", "ls")
+output = subprocess.check_output(code_1, shell=True)
+st.code(output.decode("utf-8"))
+
+"# Run another subprocess"
+code_2 = st.text_input("More code:", "ls")
+output = subprocess.check_output(code_2, shell=True)
+st.code(output.decode("utf-8"))
